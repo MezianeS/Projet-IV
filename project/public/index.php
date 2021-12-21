462dbfa69be853c10c772816ff14086b1344126b
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2021-12-21 15:48:57 +0800

    Delete project/public directory

--- project/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	require_once '../app/bootstrap.php';
-
-	// Initiation du core library
-	$init = new Core;
--- project/public/js/main.js
+++
@@ -1,4 +0,0 @@
-function dateFR($datetime) {
-    setlocale(LC_ALL, 'fr_FR');
-    return strftime('%d %B %Y &agrave; %Hh%M', strtotime($datetime));
-}
